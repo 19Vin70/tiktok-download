96bea61eb014789ee98c99c1808c4934683f21c7
19Vin70 <alqasimquillo@gmail.com>
2024-03-31 12:43:18 +0800

    New commit

--- js/script.js
+++ js/script.js
@@ -187,3 +187,58 @@ function downloadMusic(videoID) {
         })
         .catch(err => console.error('Error downloading music:', err));
 }
+
+
+
+
+
+const textarea = document.querySelector("textarea"),
+voiceList = document.querySelector(".select"),
+speechBtn = document.getElementById("speech");
+
+let synth = speechSynthesis,
+isSpeaking = true;
+
+voices();
+
+function voices(){
+    for(let voice of synth.getVoices()){
+        let selected = voice.name === "Google US English" ? "selected" : "";
+        let option = `<option value="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option>`;
+        voiceList.insertAdjacentHTML("beforeend", option);
+    }
+}
+
+synth.addEventListener("voiceschanged", voices);
+
+function textToSpeech(text){
+    let utterance = new SpeechSynthesisUtterance(text);
+    for(let voice of synth.getVoices()){
+        if(voice.name === voiceList.value){
+            utterance.voice = voice;
+        }
+    }
+    synth.speak(utterance);
+}
+
+speechBtn.addEventListener("click", e => {
+    e.preventDefault();
+    
+    if (textarea.value !== "") {
+        if (!synth.speaking) {
+            textToSpeech(textarea.value);
+        }
+        
+        if (textarea.value.length > 80) {
+            if (isSpeaking) {
+                synth.resume();
+                isSpeaking = false;
+                speechBtn.innerText = "Pause Speech";
+            } else {
+                synth.pause();
+                isSpeaking = true;
+                speechBtn.innerText = "Resume Speech";
+            }
+        }
+    }
+});
